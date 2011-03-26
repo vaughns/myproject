2022aefb3ee49a929bd2d57f65265b5ffca7d0fd
Vani Ramakrishnan <vani.ramakrishnan@gmail.com>
2011-03-26 13:58:46 -0500

    added redirect / to /polls

--- polls/views.py
+++ polls/views.py
@@ -13,3 +13,8 @@ def results(request, poll_id):
 
 def vote(request, poll_id):
     return HttpResponse("You're voting on poll %s." % (poll_id,))
+
+from django.http import HttpResponseRedirect
+
+def redirect_to_polls(request):
+	return HttpResponseRedirect('/polls/')
