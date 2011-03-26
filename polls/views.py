f6f460cba7cb2fa5d23c0d5b2529002c5df2eb07
Vani Ramakrishnan <vani.ramakrishnan@gmail.com>
2011-03-26 15:23:14 -0500

    added more functional views; index.html

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,14 @@
 # Create your views here.
 
+from django.shortcuts import render_to_response
+from polls.models import Poll
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_poll_list': latest_poll_list}
+    return render_to_response('polls/index.html', context)
 
 def detail(request, poll_id):
     return HttpResponse("You're looking at poll %s." % (poll_id,))
@@ -13,8 +18,6 @@ def results(request, poll_id):
 
 def vote(request, poll_id):
     return HttpResponse("You're voting on poll %s." % (poll_id,))
-
-from django.http import HttpResponseRedirect
 
 def redirect_to_polls(request):
 	return HttpResponseRedirect('/polls/')
